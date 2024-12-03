ef1fb70aa57e63e6531a04817c0f9e1e9a4ac69a
onetrue-6657 <nz.osu@outlook.com>
2024-12-02 21:17:15 -0500

    dynamic

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -94,16 +94,15 @@ def get_dealerships(request, state="All"):
 
 def get_dealer_reviews(request, dealer_id):
     if (dealer_id):
-        endpoint = "/fetchDealer/dealer/" + str(dealer_id)
+        endpoint = "/fetchReviews/dealer/"+str(dealer_id)
         reviews = get_request(endpoint)
-        for review_detail in reviews:
-            response = analyze_review_sentiments(review_detail['review'])
+        for review_details in reviews:
+            response = analyze_review_sentiments(review_details['review'])
             print(response)
-            review_detail['sentiment'] = response['sentiment']
+            review_details['sentiment'] = response['sentiment']
         return JsonResponse({"status": 200, "reviews": reviews})
     else:
-        return JsonResponse({"status": 400, "message": "Bad Request"})
-
+        return JsonResponse({"status": 400, "message": "Reviews not found"})
 
 def get_dealer_details(request, dealer_id):
     if (dealer_id):
@@ -114,12 +113,16 @@ def get_dealer_details(request, dealer_id):
         return JsonResponse({"status": 400, "message": "Bad Request"})
 
 def add_review(request):
-    if(request.user.is_anonymous == False):
+    if (request.user.is_anonymous is False):
         data = json.loads(request.body)
         try:
             response = post_review(data)
-            return JsonResponse({"status":200})
+            return JsonResponse({"status": 200})
         except:
-            return JsonResponse({"status":401,"message":"Error in posting review"})
+            return JsonResponse({"status": 401,
+                                 "message": "Error in posting review"})
+        finally:
+            print("add_review request successful!")
     else:
-        return JsonResponse({"status":403,"message":"Unauthorized"})
+        return JsonResponse({"status": 403,
+                             "message": "Unauthorized"})
