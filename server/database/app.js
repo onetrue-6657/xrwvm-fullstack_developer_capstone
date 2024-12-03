a40e72949fed66ccca274f577bfc0cea9def6eb8
onetrue-6657 <nz.osu@outlook.com>
2024-12-02 21:38:50 -0500

    linnt

--- server/database/app.js
+++ server/database/app.js
@@ -10,7 +10,7 @@ app.use(require("body-parser").urlencoded({ extended: false }));
 
 const reviews_data = JSON.parse(fs.readFileSync("reviews.json", "utf8"));
 const dealerships_data = JSON.parse(
-  fs.readFileSync("dealerships.json", "utf8"),
+  fs.readFileSync("dealerships.json", "utf8")
 );
 
 mongoose.connect("mongodb://mongo_db:27017/", { dbName: "dealershipsDB" });
